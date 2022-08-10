5529056937fb0d3fee488c7b33a470284a63578b
Douglas I F <difnandes07@gmail.com>
2022-08-10 10:21:26 -0300

    first commit

---
+++ escola/serializer.py
@@ -0,0 +1,37 @@
+from dataclasses import field
+from imp import source_from_cache  #verificar !!!!!!!!!!!!!!
+from pyexpat import model  
+from dataclasses import fields  
+from rest_framework import serializers
+from escola.models import Aluno, Curso, Matricula
+
+
+class AlunoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Aluno
+        fields = '__all__' # verificar !!!!!!!!!!!
+        
+class CursoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Curso
+        fields = '__all__'
+
+class MatriculaSerializer(serializers.ModelSerializer):
+    class Meta:
+       model = Matricula
+       exclude = [] 
+       
+class ListaMatriculasAlunoSerializer(serializers.ModelSerializer):
+    curso = serializers.ReadOnlyField(source='curso.descricao')
+    periodo = serializers.SerializerMethodField()
+    class Meta:
+        model = Matricula
+        fields = ['curso', 'periodo']
+    def get_periodo(self, obj):
+        return obj.get_periodo_display()
+    
+class ListaAlunosMatriculadosSerializer(serializers.ModelSerializer):
+    aluno_nome = serializers.ReadOnlyField(source='aluno.nome')
+    class Meta:
+        model = Matricula
+        fields = ['aluno_nome']
